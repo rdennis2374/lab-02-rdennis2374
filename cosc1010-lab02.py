10118d02d61d6c84d5af61a71cf02c1da031d4c8
Danny Radosevich <deradosevich@gmail.com>
2024-08-19 11:59:58 -0600

    Add files via upload

---
+++ cosc1010-lab02.py
@@ -0,0 +1,48 @@
+# Your Name Here
+# UWYO COSC 1010
+# Submission Date
+# Lab 02 
+# Lab Section: 
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+your_variable_here = "when you see this, replace it with your code"
+
+## Section ONE
+
+# Complete the following print statement to print out "Hello, COSC1010"
+print()
+
+# Assign the string above to a variable named hello_message and print that variable
+hello_message = ""
+print("Your code here")
+
+# Assign the string "cowboy joe" to a variable, and print that variable with title casing
+print("Your code here")
+
+# Complete the following f-string print message 
+    # You will need to create your own variables and insert them  
+    # the final message should read `The University of Wyoming was founded in 1886`
+
+print(f"The {your_variable_here} was founded in {your_variable_here}")
+
+# Now let's do some math with variables 
+    # Create two variables x and y and assign them the values 5 and 10 respectively 
+    # Complete the following print statements using your variables
+    #All math must be done within the braces in the f-strings
+print(f"x + y = {your_variable_here}")
+print(f"x - y = {your_variable_here}")
+print(f"x * y = {your_variable_here}")
+print(f"x / y = {your_variable_here}")
+
+# String concatenation 
+    # Finally we will take a look at string concatenation
+    # String concatenation combines two strings together
+    # It is done using the + operator
+    # Create three variables:
+        # first_name, which is your first name 
+        # last_name, which is your last name
+        # space, which is a space character 
+    # Use string concatenation to print out your full name 
